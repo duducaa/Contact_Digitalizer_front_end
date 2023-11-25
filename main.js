22c1fcf81cb70452caf0361858d30be31dd05179
duducaa <duduca1881@gmail.com>
2023-11-25 19:14:05 -0300

    the front end can send single images for the api

--- main.js
+++ main.js
@@ -1,46 +1,72 @@
 export const URL_BASE = "http://localhost:5000";
 
-const fileInput = document.querySelector("#file-insert");
-const image = document.querySelector(".image");
-const imageName = document.querySelector(".image-name");
-const prevImage = document.querySelector(".prev-image");
-const nextImage = document.querySelector(".next-image");
-const sendImages = document.querySelector(".send-images");
+$(document).ready(() => {
+    const fileInput = $("#file-insert");
+    const image = $(".image");
+    const imageName = $(".image-name");
+    const prevImage = $(".prev-image");
+    const nextImage = $(".next-image");
+    const sendImagesButton = $(".send-images");
 
-let actualImageIndex = 0;
-let url = null;
+    let actualImageIndex = 0;
+    let url = null;
+    let files = [];
 
-fileInput.addEventListener('change', () => {
-    const files = fileInput.files;
-    if (files.length > 1) {
-        prevImage.classList.remove("d-none");
-        nextImage.classList.remove("d-none");
-    }
-    url = URL.createObjectURL(fileInput.files[0]);
-    imageName.textContent = fileInput.files[actualImageIndex].name;
-    image.src = url;
+    fileInput.on('change', (event) => {
+        files = event.target.files;
+        if (files.length > 1) {
+            prevImage.removeClass("d-none");
+            nextImage.removeClass("d-none");
+        }
+        url = URL.createObjectURL(files[0]);
+        imageName.html(files[actualImageIndex].name);
+        image.attr("src", url);
+    });
+
+    prevImage.on('click', () => {
+        if (actualImageIndex > 0) {
+            actualImageIndex--;
+            URL.revokeObjectURL(url);
+            url = URL.createObjectURL(files[actualImageIndex]);
+            imageName.html(files[actualImageIndex].name);
+            image.attr("src", url);
+        }
+    });
+
+    nextImage.on('click', () => {
+        if (actualImageIndex < files.length - 1) {
+            actualImageIndex++;
+            URL.revokeObjectURL(url);
+            url = URL.createObjectURL(files[actualImageIndex]);
+            imageName.html(files[actualImageIndex].name);
+            image.attr("src", url);
+        }
+    });
+
+    sendImagesButton.on('click', () => {
+        const file = files[0];
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const image = e.target.result.split(',')[1];
+
+            sendImages({image: image}).then(response => {
+                console.log(response['contacts']);
+            });
+        }; 
+        reader.readAsDataURL(file);
+    });
 });
 
-prevImage.addEventListener('click', () => {
-    if (actualImageIndex > 0) {
-        actualImageIndex--;
-        URL.revokeObjectURL(url);
-        url = URL.createObjectURL(fileInput.files[actualImageIndex]);
-        imageName.textContent = fileInput.files[actualImageIndex].name;
-        image.src = url;
-    }
-});
-
-nextImage.addEventListener('click', () => {
-    if (actualImageIndex < fileInput.files.length - 1) {
-        actualImageIndex++;
-        URL.revokeObjectURL(url);
-        url = URL.createObjectURL(fileInput.files[actualImageIndex]);
-        imageName.textContent = fileInput.files[actualImageIndex].name;
-        image.src = url;
-    }
-});
-
-sendImages.addEventListener('click', () => {
-    
-})
+function sendImages(image) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            type: "POST",
+            url: URL_BASE + "/detection",
+            data: JSON.stringify([image]),
+            dataType: "json",
+            contentType: "application/json",
+            success: (response) => resolve(response),
+            error: (response) => reject(response)
+        });
+    });
+}
