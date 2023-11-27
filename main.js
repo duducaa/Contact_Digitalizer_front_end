79ba942bfa766c3e9df5a6c38b90462dec1aaea0
duducaa <duduca1881@gmail.com>
2023-11-27 00:45:27 -0300

    interface finished for a single image request

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ $(document).ready(() => {
     const prevImage = $(".prev-image");
     const nextImage = $(".next-image");
     const sendImagesButton = $(".send-images");
+    const darkBg = $(".dark-bg");
+    const detections = $(".detections");
+    const content = detections.find(".content");
+    const closeBox = $(".close-box");
+    const header = $(".header");
 
     let actualImageIndex = 0;
     let url = null;
@@ -16,7 +21,10 @@ $(document).ready(() => {
         files = event.target.files;
         if (files.length > 1) {
             prevImage.removeClass("d-none");
-            nextImage.removeClass("d-none");
+            nextImage.removeClass("d-none");            
+        }
+        if (files.length > 0) {
+            header.removeClass("d-none");
         }
         url = URL.createObjectURL(files[0]);
         imageName.html(files[actualImageIndex].name);
@@ -50,12 +58,48 @@ $(document).ready(() => {
             const image = e.target.result.split(',')[1];
 
             sendImages({image: image}).then(response => {
-                console.log(response['contacts']);
+                const contacts = response.contacts;
+                Object.keys(contacts).forEach(key => {
+                    const contact = createInfo(key, contacts[key]);
+                    content.append(contact);
+                });
+                darkBg.removeClass("d-none");
+                detections.removeClass("d-none").addClass("d-flex");
+                console.log(response);
             });
-        }; 
+        };
         reader.readAsDataURL(file);
     });
+
+    closeBox.on("click", () => {
+        content.html("");
+        darkBg.addClass("d-none");
+        detections.addClass("d-none");
+        header.addClass("d-none");
+        fileInput.val("");
+        image.attr("src", "");
+    });
 });
+
+function createInfo(key, values) {
+    const info = $('<div>');
+    info.append($('<h2>', {
+        'html': key
+    }));
+
+    const contacts = $('<div>');
+
+    values.forEach(value => {
+        contacts.append($('<p>', {
+            'html': value[0],
+            'class': 'd-block'
+        }));
+    });
+    
+    contacts.appendTo(info);
+
+    return info;
+}
 
 function sendImages(image) {
     return new Promise((resolve, reject) => {
